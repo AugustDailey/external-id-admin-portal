df56d6f36d0dc52081072a148f2732582af6223a
August Dailey <August.1441@live.com>
2025-08-05 07:56:21 -0500

    Improve styling and allow for flexible toggling between search and save

--- src/Pages/UserLookup.cshtml.cs
+++ src/Pages/UserLookup.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.Graph;
 using Azure.Identity;
 using Microsoft.Extensions.Options;
 using src.Options;
+using Microsoft.Graph.Models;
 
 namespace src.Pages
 {
@@ -12,6 +13,7 @@ namespace src.Pages
         [BindProperty]
         public string UPN { get; set; }
 
+        [BindProperty]
         public Dictionary<string, string> UserAttributes { get; set; }
         public GraphOptions GraphOptions { get; set; }
         public UserLookupModel(IOptions<GraphOptions> graphOptions)
@@ -19,7 +21,7 @@ namespace src.Pages
             GraphOptions = graphOptions.Value;
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostSearchAsync()
         {
             if (string.IsNullOrWhiteSpace(UPN))
                 return Page();
@@ -48,13 +50,42 @@ namespace src.Pages
             catch (ServiceException ex)
             {
                 UserAttributes = new Dictionary<string, string>
-            {
-                { "Error", ex.Message }
-            };
+                {
+                    { "Error", ex.Message }
+                };
             }
 
             return Page();
         }
+
+        public async Task<IActionResult> OnPostSaveAsync()
+        {
+            var graphClient = new GraphServiceClient(new ClientSecretCredential(GraphOptions.TenantId, GraphOptions.ClientId, GraphOptions.ClientSecret));
+
+            var userUpdate = new User
+            {
+                DisplayName = UserAttributes["Display Name"],
+                GivenName = UserAttributes["First Name"],
+                Surname = UserAttributes["Last Name"],
+                Department = UserAttributes["Department"],
+                AdditionalData = new Dictionary<string, object>
+                {
+                    { "extension_52104e8f53e04ca29658b024fba16661_userType", UserAttributes["User Type"] }
+                }
+            };
+
+            try
+            {
+                await graphClient.Users[UPN].PatchAsync(userUpdate);
+            }
+            catch (ServiceException ex)
+            {
+                ModelState.AddModelError(string.Empty, $"Error updating user: {ex.Message}");
+            }
+
+            return await OnPostSearchAsync(); // Or return Page() to stay on the same page
+        }
+
     }
 }
 
